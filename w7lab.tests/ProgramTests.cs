5c281d599360cffa2f3f355cdb6353b2cde39d96
redista <sealacquire@gmail.com>
2021-03-23 11:40:45 +0000

    Add project files.

---
+++ w7lab.tests/ProgramTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using w7lab;
+
+namespace w7lab.tests
+{
+    // TODO: Replace examples and use TDD development by writing your own tests
+
+    [TestFixture]
+    public class CustomMathTest
+    {
+        static int ExpectedResult;
+
+        [SetUp]
+        public static void Init()
+        {
+        }
+
+        [Test]
+        public void Multiply_1()
+        {
+            ExpectedResult = 20;
+            int ActualResult = Program.CustomMath.multiply(4, 5);
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+
+        [Test]
+        public void Multiply_2()
+        {
+            ExpectedResult = 0;
+            int ActualResult = Program.CustomMath.multiply(0, 5);
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+
+        [Test]
+        public void Multiply_3()
+        {
+            ExpectedResult = -25;
+            int ActualResult = Program.CustomMath.multiply(5, 5);
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+    }
+
+    [TestFixture]
+    public class DivisibleNbTest
+    {
+        static bool ExpectedResult;
+        bool ActualResult;
+
+        [SetUp]
+        public static void Init()
+        {
+        }
+
+        [Test]
+        public void DivisbleTest_1()
+        {
+            ExpectedResult = true;
+            ActualResult = DivisibleNb.isDivisible(5, 5, 5);
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+
+        [Test]
+        public void DivisbleTest_2()
+        {
+            ExpectedResult = true;
+            ActualResult = DivisibleNb.isDivisible(5, 5, 5);
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+
+        [Test]
+        public void DivisbleTest_3()
+        {
+            ExpectedResult = true;
+            ActualResult = DivisibleNb.isDivisible(-10, 0, 10);
+            Assert.Throws<DivideByZeroException>(DivisibleNb.isDivisible(-10, 0, 10), );
+        }
+    }
+}
---
+++ w7lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w7lab
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class CustomMath
+        {
+            public static int multiply(int a, int b)
+            {
+                return a * b;
+            }
+        }
+    }
+
+    public class DivisibleNb
+    {
+        public static bool isDivisible(long n, long x, long y)
+        {
+            return (n / x == 0 && n / y == 0);
+        }
+    }
+
+}
+
